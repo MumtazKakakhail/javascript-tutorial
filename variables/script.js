2a073b66285f0e2e5d72646d0dd82f53c6e0d58e
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-11 18:53:29 +0500

    javascript tutorial

--- variables/script.js
+++ variables/script.js
@@ -1,3 +1,10 @@
 fullName = "shams";
 age = 25;
 console.log(fullName);
+x = null ;
+y = undefined;
+console.log(x);
+isFollow = true;   //boolean = true or false
+console.log(isFollow);
+isFollow = false; //boolean = true or false
+console.log(isFollow);
