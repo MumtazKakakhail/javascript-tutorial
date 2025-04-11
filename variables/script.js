91ad4e3a2e34de5d20f2b88c992e50f1ddabf9ab
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-11 19:37:04 +0500

    javascript tutorial

--- variables/script.js
+++ variables/script.js
@@ -1,5 +1,5 @@
 fullName = "shams";
-age = 25;
+// age = 25;
 console.log(fullName);
 x = null ;
 y = undefined;
@@ -8,3 +8,15 @@ isFollow = true;   //boolean = true or false
 console.log(isFollow);
 isFollow = false; //boolean = true or false
 console.log(isFollow);
+//var
+ //var = variable name  
+var gender = "female"; 
+var gender = "shemale"; 
+var gender = "male";
+console.log(gender);   
+//let
+let age = 25; //let = variable name
+console.log(age);
+//const     
+const cnicNo = 255655345565; //const = variable name
+console.log(cnicNo);
