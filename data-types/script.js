325285832d8d4a6ac956956cc0e390efe8468986
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-12 23:24:06 +0500

    data types in javascript

--- data-types/script.js
+++ data-types/script.js
@@ -3,8 +3,8 @@
 //1. Primitive data types
 //Number
 let age = 21;
- console.log(age); 
- console.log(typeof age); 
+console.log(age);
+console.log(typeof age);
 //typeof = it tells the data type of the variable
 //String
 let fullName = "shams";
@@ -12,11 +12,11 @@ console.log(fullName);
 console.log(typeof fullName);
 //Boolean
 let isFollow = true; //boolean = true or false
-console.log(isFollow); 
+console.log(isFollow);
 console.log(typeof isFollow);
 //Null
 let x = null; //null = no value 
-console.log(x); 
+console.log(x);
 console.log(typeof x); //object
 //Undefined
 let y; //undefined = no value
@@ -34,17 +34,17 @@ console.log(typeof symbol); //symbol
 //2. Non-primitive data types
 //Object
 const student = {
-    fullName : "Basharat",
-    age : 21,
-    rollNo : 12,
-    cgpa : 3.5,
-    isPass : true,
+    fullName: "Basharat",
+    age: 21,
+    rollNo: 12,
+    cgpa: 3.5,
+    isPass: true,
 };
-console.log(student); 
+console.log(student);
 console.log(student.age); //accessing object property
 console.log(student["fullName"]); //accessing object property
 console.log(typeof student);
-student["age"]=student["age"] + 1 ;//updating object property
-console.log(student.age); 
+student["age"] = student["age"] + 1;//updating object property
+console.log(student.age);
 student["fullName"] = "Ali"; //updating object property
 console.log(student["fullName"]);
