025e59ce14d7f0fe9d7fd776b882842871430e53
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-15 22:12:38 +0500

    Operators in javascript

---
+++ operators/script.js
@@ -0,0 +1,9 @@
+// Arithmatic Operators
+let a = 11;
+b = 13;
+console.log("a =", a, "& b =", b);
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
