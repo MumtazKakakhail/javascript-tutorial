5e85e9c15e6534c6ee0b625db67809b722d5c4a4
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-15 22:45:54 +0500

    Operators in javascript

--- operators/script.js
+++ operators/script.js
@@ -7,3 +7,35 @@ console.log("a + b =", a + b);
 console.log("a - b =", a - b);
 console.log("a * b =", a * b);
 console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+//Unary operators
+let c = 5;
+d = 7;
+console.log("c =", c, "d =", d);
+c++;
+console.log("c =", c);
+d--;
+console.log("d =",d);
+++c;
+console.log("++c =", ++c);
+
+//Assignment Operators
+//= , += , -= , *= , %= , **= , /=
+let x = 3;
+x += 4;
+console.log("x =", x);
+x -= 2;
+console.log("x =", x);
+x *= 5;
+console.log("x =", x);
+x %= 4;
+console.log("x =", x);
+x **= 2;
+console.log("x =", x);
+x /= 4;
+console.log("x =", x);
+
+//Comparision Operators
+// ==, ===, !=, !===, >, >=, <, <=
