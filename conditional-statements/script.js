f118864a8bdd86d4c4416cf82e6c2b804ee49d97
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-16 18:43:55 +0500

    conditional statements in javascript

--- conditional-statements/script.js
+++ conditional-statements/script.js
@@ -1,3 +1,5 @@
+//Conditional Statements
+//If Statement
 let age = 21;
 if (age >= 18) {
     console.log("You can vote");
@@ -5,5 +7,46 @@ if (age >= 18) {
 }
 if (age < 18) {
     console.log("You can not vote");
+    
+}
+
+// let mode = "dark";
+// let color;
+// if (mode === "dark") {
+//     color = "black";
+    
+// }
+// if (mode === "light") {
+//     color = "white";
+// }
+// console.log(color);
+
+
+//If-else Statement
+let mode = "light";
+let color;
+if (mode === "dark") {
+    color = "black";
+} else {
+    color = "white";
+}
+console.log(color);
+
+
+let Age = 17;
+if (Age >= 18) {
+    console.log("Eligible to vote");
+    
+} else {
+    console.log("Not eligible to vote");
+    
+}
+
+let num = 6;
+if (num%2 === 0) {
+    console.log("Even");
+    
+} else {
+    console.log("Odd");
     
 }
