33a98b7091c9e294da3b6817e2cf0627047cddbc
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-24 22:28:50 +0500

    loops and strings in javascript

--- loops-and-strings/loops/script.js
+++ loops-and-strings/loops/script.js
@@ -55,7 +55,31 @@ for (let key in student) {
 
 
 //practice Question
-// 1- print all even numbers from 0 to 100
+// 1- print all numbers from 0 to 100
 for (let num = 0; num <= 100; num++) {
   console.log("num", num);
 }
+
+// 2- print all even numbers from 0 to 100
+for (let num = 0; num <= 100; num++) {
+if (num % 2 === 0) {
+  console.log("num =", num);
+  
+}
+}
+
+// 3- print all odd numbers from 0 to 100
+for (let num = 0; num <= 100; num++) {
+  if (num % 2 !== 0) {
+    console.log("num =", num);
+    
+  }
+  
+}
+
+let gameNum = 3;
+let userNum = prompt("guess the number:");
+while (userNum != gameNum) {
+  userNum = prompt("you guess the wrong number, try again!")
+}
+console.log("Congratulations you enter the right number!");
---
+++ loops-and-strings/strings/script.js
@@ -0,0 +1,20 @@
+let str = "hi!";
+console.log(str);
+
+let str1 = "hello!";
+console.log(str1);
+console.log(str1[3]);
+
+//template literal
+let template = `hello! this is a string`;
+console.log(template);
+console.log(typeof template);
+
+let obj = {
+    item: "pen",
+    price: 10,
+};
+let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
+
+console.log("the cost of", obj.item, "is", obj.price, "rupees");
