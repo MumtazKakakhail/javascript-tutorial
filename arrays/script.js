d95e7c4fd33a4549088f231062ef4000651ec360
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-29 22:51:02 +0500

    Arrays in javascript

--- arrays/script.js
+++ arrays/script.js
@@ -2,3 +2,24 @@ let students = ["Mumtaz","Shahab","Kamran","Yasir","Raza","Basharat"]
 console.log(students);
 let marks = [95,92,89,99,78]
 console.log(marks);
+
+// for loop
+let fruits = ["mango","banana","apple","orange","waterlemon","cherry"]
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+    
+    
+}
+
+//for-of
+let std = ["atif","shafiq","Karamat","ejaz"]
+for (const element of std) {
+    console.log(element);
+    
+}
+
+let cities = ["Gilgit","Islamabad","Lahore","Karachi"]
+for (const city of cities) {
+    console.log(city);
+    
+}
