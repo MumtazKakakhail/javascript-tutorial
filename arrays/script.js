62b6dd3cbaf56e4cf74138512da5d91e5a5f11bd
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-04-30 19:22:10 +0500

    Arrays and arrays methods in javascript

--- arrays/script.js
+++ arrays/script.js
@@ -20,3 +20,25 @@ for (const city of cities) {
   console.log(city);
 }
 
+//Practice questions
+let Marks = [60,45,74,55,67,52]
+let sum = 0;
+for (const val of Marks) {
+  sum += val;
+
+}
+let avg = sum / Marks.length;
+console.log(`average marks of class = ${avg}`);
+
+//for a given array with prices of items => [250,645,300,900,50]
+//All items have an offer of 10% off on them. change the array to store final price after applying the offer.
+let items = [250,645,300,900,50];
+console.log(items);
+
+for (let i = 0; i < items.length; i++) {
+ let offer = items[i] / 10;
+items[i] -= offer;
+
+}
+console.log(items);
+
