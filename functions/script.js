50fc8cef9384bdef283082d20ede2ac53663f218
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-05 13:22:29 +0500

    functions

--- functions/script.js
+++ functions/script.js
@@ -1,19 +1,17 @@
 //Function definition
 function myFunction() {
-    console.log("Hello!");
+  console.log("Hello!");
 }
 myFunction();
 
 function hello() {
-    console.log("welcome!");
-    
+  console.log("welcome!");
 }
 hello();
 hello();
 hello();
 
-function sum(x,y) {
-    console.log(4 + 6);
-    
+function sum(x, y) {
+  console.log(4 + 6);
 }
 sum();
