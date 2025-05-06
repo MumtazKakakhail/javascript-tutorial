225c3d49e2861e056536297b78e6ab0b83a3d907
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-06 23:19:08 +0500

    Functions in javascript

--- functions/script.js
+++ functions/script.js
@@ -71,7 +71,7 @@ let Num = [4,65,7845,32,3];
 Num.forEach((val)=> {
   console.log(val);
   
-})
+});
 
 // const fruits = ["apple", "banana", "cherry"];
 
@@ -83,7 +83,7 @@ const fruits = ["apple", "banana", "cherry"];
 fruits.forEach((val) => {
   console.log(val.toUpperCase());
   
-})
+});
 
 //practice
 //for a given number of arrays, print the square of each value using forEach loop.
@@ -91,6 +91,38 @@ let nums = [2,3,5,6,11,13]
 nums.forEach((num) => {
   console.log(num**2);
   
-})
+});
 
 //map 
+// let marks = [34,43,36,45];
+// marks.map((val) => {
+//   console.log(val);
+  
+// });
+let marks = [34,43,36,45];
+let newMarks = marks.map((val) => {
+return val + 10;
+  
+});
+console.log(newMarks);
+
+
+//reduce 
+let Arr = [1,5,6,3,9];
+const output = Arr.reduce((prev, curr) => {
+  return prev + curr ;
+});
+console.log(output);
+
+
+let Res = [4,67,478,2];
+let Output = Res.reduce((prev, curr) => {
+  return prev > curr ? prev : curr;
+});
+console.log(Output);
+
+let Marks = [67,98,92,63,78,91,82];
+const toppers = Marks.filter((val) => {
+  return val > 90;
+});
+console.log(toppers);
