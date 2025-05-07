787809e86df2c32fc4c123db4ac58ffe26a2c7e8
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-07 20:23:43 +0500

    global,local,block and function scope in javascript

--- scope/script.js
+++ scope/script.js
@@ -1,8 +1,37 @@
 //Scope
 //Global scope
-let age = 21;
+let age =21;
 function stdAge() {
-    console.log(age);
+    console.log("age:", age);
 }
 stdAge();
-console.log(age);
+console.log("age:",age);
+
+
+//Local Scope (Function Scope)
+function stdRollNo() {
+    let rollNo = 12;
+    console.log("rollNo:",rollNo);
+    
+}
+stdRollNo();
+
+
+// Block Scope
+if (true) {
+    let result = "pass";
+    console.log(result);
+    
+}
+
+// Function Scope
+function final() {
+    let finalMarks = 460;
+    console.log(finalMarks);
+    if (true) {
+        console.log(finalMarks);
+    }
+   
+}
+final();
+
