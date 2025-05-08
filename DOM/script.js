1dc9833b4c3421ce0833c485c0e545856c594cd8
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-08 21:03:17 +0500

    DOM Manipulation

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+//DOM Manipulation
+//get element by ID
+let Heading = document.getElementById("heading");
+console.log(Heading);
+
+//get element by class
+let paragraph = document.getElementsByClassName("para");
+console.log(paragraph);
+console.dir(paragraph);
+
+
+//get element by tagname
+let paras = document.getElementsByTagName("p");
+console.log(paras);
+console.dir(paras);
+ 
